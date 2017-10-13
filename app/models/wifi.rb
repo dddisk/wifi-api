0b6ba5d53448f440492cd9428a9c49afdbcf8ce1
dddisk <13da144@gmail.com>
2017-10-13 18:45:47 +0900

    make geocoder config

--- app/models/wifi.rb
+++ app/models/wifi.rb
@@ -1,4 +1,4 @@
 class Wifi < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode, :if => :address_changed?
+  after_validation :geocode
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'wifis' => 'wifis#index'
+  root "wifis#index"
 end
