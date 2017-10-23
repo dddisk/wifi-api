728ba87759d2afe72141dcbcfba3187404381026
dddisk <13da144@gmail.com>
2017-10-24 00:01:13 +0900

    データを絞り込んだ

--- app/controllers/wifis_controller.rb
+++ app/controllers/wifis_controller.rb
@@ -1,27 +1,40 @@
 class WifisController < ApplicationController
 
   def index
-    if params[:search].present?
-      para = Wifi.near(params[:search], 1)
-      binding.pry
+    if wifi_params.present?
+    address = wifi_params[:address]
+
+    #距離のデフォルト指定
+    distance = (wifi_params[:distance].to_f / 1000)
+    if distance.zero?
+      distance = 0.5
+    end
+
+    #件数のデフォルト指定
+    limit = wifi_params[:limit].to_f
+    if limit.zero?
+      limit = 5
+    end
+
+    @locations = Wifi.alteration(address,distance,limit)
+    binding.pry
+
 
     end
+
+
   end
 
   def new
   end
 
   def create
-    params = wifi_params
-    @params = wifi_params.values
-    @prefecture = params[:prefecture]
-    @address = params[:latitude].to_i + params[:longitude].to_i
   end
 
 
   private
 
   def wifi_params
-    params.permit(:prefecture, :latitude, :longitude)
+    params.permit(:address, :distance, :limit)
   end
 end
--- app/models/wifi.rb
+++ app/models/wifi.rb
@@ -2,4 +2,10 @@ class Wifi < ApplicationRecord
   attr_accessor :address, :latitude, :longitude
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
+
+
+  def self.alteration(ad,di,li)
+    locations = near(ad, di, :order => :distance).limit(li)
+  end
+
 end
