90869031e443965a36e1c3175a12459356f103b7
dddisk <13da144@gmail.com>
2017-10-24 20:01:49 +0900

    clean code & todolist

--- app/controllers/wifis_controller.rb
+++ app/controllers/wifis_controller.rb
@@ -3,32 +3,7 @@ class WifisController < ApplicationController
 
 
   def index
-    # if wifi_params.present?
-    # address = wifi_params[:address]
 
-    # #距離のデフォルト指定
-    #   distance = (wifi_params[:distance].to_f / 1000)
-    #   if distance.zero?
-    #     distance = 0.5
-    #   end
-
-    #   #件数のデフォルト指定
-    #   limit = wifi_params[:limit].to_f
-    #   if limit.zero?
-    #     limit = 5
-    #   end
-
-    #   #@locations[0][:address]みたいなのは取れる
-    #   @locations = Wifi.alteration(address,distance,limit)
-    #   @locations = @locations.map do |loca|
-    #     loca = {
-    #       name: loca.name,
-    #       address: loca[:address],
-    #       distance_m: (loca.distance * 1000).round(1)
-    #     }
-    #   end
-    # end
-    # render json: @locations
   end
 
   def new
@@ -53,7 +28,7 @@ class WifisController < ApplicationController
         limit = 5
       end
 
-      #@locations[0][:address]みたいなのは取れる
+
       @locations = Wifi.alteration(address,distance,limit)
       @locations = @locations.map do |loca|
         loca = {
